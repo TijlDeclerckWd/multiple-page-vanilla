6ecf690f887070af28dac58931fdc41f0ee48028
Tijl Declerck <tijl.declerck@outlook.com>
2023-07-16 19:57:37 +0700

    change links

--- src/js/about.js
+++ src/js/about.js
@@ -1,9 +1,2 @@
 // JavaScript code for about.html
 console.log('This is the about page.');
-
-// write me a function that takes an array of numbers and returns the sum of the numbers, use reduce
-
-// write me a function that takes an array of numbers and returns the sum of the numbers, use reduce
-function sumArray(arr) {
-    return arr.reduce((acc, val) => acc + val, 0);
-}
